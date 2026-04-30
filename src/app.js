fb076fcd8d2595645d564bb23864de9ce5e3464a
vatsakshat02 <vatsakshat02@gmail.com>
2026-04-30 13:15:24 +0530

    feat: added middlewares

--- src/app.js
+++ src/app.js
@@ -2,17 +2,18 @@ const express = require("express");
 
 const app = express();
 
-app.use(
-  "/user",
-  (req, res, next) => {
-    res.send("response 1");
-    next();
-  },
-  (req, res, next) => {
-    res.send("response 2");
-    next();
-  }
-);
+app.use("/", (req, res, next) => {
+  next();
+});
+
+app.use("/user", (req, res, next) => {
+  // res.send("response 1");
+  next();
+});
+
+app.use("/user", (req, res, next) => {
+  res.send("response 2");
+});
 
 app.listen(3000, () => {
   console.log("server is listening on port 3000");
