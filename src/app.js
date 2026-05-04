c2ab7bc611a9ad8bfd8de5760b13a963bba478c2
vatsakshat02 <vatsakshat02@gmail.com>
2026-05-04 17:50:36 +0530

    feat: added dynamic signup

--- src/app.js
+++ src/app.js
@@ -1,6 +1,23 @@
 const express = require("express");
 const connectDB = require("./config/database");
 const app = express();
+const User = require("./models/user");
+
+app.post("/signup", async (req, res) => {
+  //creating a new instance of the user model
+  const user = new User({
+    firstName: "Akshat",
+    lastName: "vats",
+    mailID: "abc@gmail.com",
+    password: "Akshat@123",
+  });
+  try {
+    await user.save();
+    res.send("user added successfully");
+  } catch (err) {
+    res.status(400).send("Error saving the user: " + err.message);
+  }
+});
 
 connectDB()
   .then(() => {
