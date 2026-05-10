3e88b4882200b6dce02b67fef74e58885bfb2d9f
vatsakshat02 <vatsakshat02@gmail.com>
2026-05-10 13:33:59 +0530

    feat: data sanitization

--- src/app.js
+++ src/app.js
@@ -54,10 +54,23 @@ app.delete("/user", async (req, res) => {
 });
 
 //Update API - to update an user in the database
-app.patch("/user", async (req, res) => {
-  const userId = req.body.userId;
+app.patch("/user:userId", async (req, res) => {
+  const userId = req.params?.userId;
   const data = req.body;
   try {
+    const ALLOWED_UPDATES = ["userId", "photoUrl", "skills", "gender", "age"];
+
+    const isUpdateAllowed = Object.keys(data).every((k) =>
+      ALLOWED_UPDATES.includes(k)
+    );
+
+    if (!isUpdateAllowed) {
+      throw new Error("Update not allowed");
+    }
+
+    if (data?.skills.length > 10) {
+      throw new Error("Skills cannot be more than 10");
+    }
     await User.findByIdAndUpdate(userId, data, {
       returnDocument: "after",
       runValidators: true,
